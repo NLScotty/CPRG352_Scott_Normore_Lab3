03219b482eb4f1d7db0d8f0dc266e6dd108788ba
sbn073 <Scott.B.Normore@gmail.com>
2022-01-28 11:26:27 -0700

    Basic Functionality Implemented
    Bare bones requirements for the lab assignment

--- NoteTaker/src/main/java/ca/sait/notetaker/NoteServlet.java
+++ NoteTaker/src/main/java/ca/sait/notetaker/NoteServlet.java
@@ -4,6 +4,8 @@
  */
 package ca.sait.notetaker;
 
+import ca.sait.notetaker.javabeans.Note;
+import java.io.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -22,13 +24,55 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/WEB-INF/viewNote.jsp");
+        String query = request.getQueryString();
         
+        if (query != null && query.contains("edit")){
+            String path = getServletContext().getRealPath("/WEB-INF/noteFile.txt");
+            BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+            
+            String title = br.readLine();
+            String contents = br.readLine();
+
+            Note note = new Note(title,contents);
+
+            request.setAttribute("note", note);
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/editNote.jsp").forward(request,response);
+        }else {
+            String path = getServletContext().getRealPath("/WEB-INF/noteFile.txt");
+            BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+
+            String title = br.readLine();
+            String contents = br.readLine();
+
+            Note note = new Note(title,contents);
+
+            request.setAttribute("note", note);
+
+            getServletContext().getRequestDispatcher("/WEB-INF/viewNote.jsp").forward(request,response);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/WEB-INF/viewNote.jsp");
+        String title = request.getParameter("title");
+        String content = request.getParameter("content");
+        
+        
+        System.out.println(content.replace("/n", ""));
+        
+        String path = getServletContext().getRealPath("/WEB-INF/noteFile.txt");
+        
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path,false)));
+        pw.println(title);
+        pw.println(content);
+        
+        pw.close();
+        
+        Note note = new Note(title, content);
+        request.setAttribute("note",note);
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/viewNote.jsp").forward(request, response);
     }
 }
--- NoteTaker/src/main/java/ca/sait/notetaker/javabeans/Note.java
+++ NoteTaker/src/main/java/ca/sait/notetaker/javabeans/Note.java
@@ -1,9 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ca.sait.notetaker.javabeans;
 
+package ca.sait.notetaker.javabeans;
 import java.io.Serializable;
 
 /**
@@ -11,5 +7,34 @@ import java.io.Serializable;
  * @author Scott
  */
 public class Note implements Serializable {
+    private String title;
+    private String contents;
+    
+    public Note(){
+        this.title = "";
+        this.contents = "";
+    }
+    
+    public Note(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+    
     
 }
